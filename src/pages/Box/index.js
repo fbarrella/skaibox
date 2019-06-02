ecb4498e53dfbf989d8c3c03d429dcda1a0af7af
Felipe B. Netto <felipe.netto@mobiauto.com.br>
2019-06-02 03:16:10 -0300

    configured and prepared box to upload files via api using dropzone

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import api from "../../services/api";
 import { distanceInWords } from "date-fns";
 import pt from "date-fns/locale/pt";
+import Dropzone from "react-dropzone";
 
 import { MdInsertDriveFile } from "react-icons/md";
 import logo from "../../assets/skaibox.svg";
@@ -12,11 +13,22 @@ export default class Box extends Component {
         box: {}
     };
 
-    async componentDidMount() {
+    async componentDidMount () {
         const box = this.props.match.params.id;
         const response = await api.get(`skaibox/${box}`);
 
         this.setState({ box: response.data });
+    }
+
+    handleUpload = async (files) => {
+        const box = this.props.match.params.id;
+
+        files.forEach(file => {
+            const data = new FormData();
+            data.append("file", file);
+            console.log(box);
+            api.post(`skaibox/${box}/file`, data);
+        });
     }
 
     render() {
@@ -27,9 +39,19 @@ export default class Box extends Component {
                     <h1>{ this.state.box.title }</h1>
                 </header>
 
+                <Dropzone onDropAccepted={this.handleUpload}>
+                    {({ getRootProps, getInputProps }) => (
+                        <div className="upload" { ... getRootProps() }>
+                            <input { ... getInputProps() } />
+
+                            <p>Arraste os arquivos ou clique aqui!</p>
+                        </div>
+                    )}
+                </Dropzone>
+
                 <ul>
                     { this.state.box.files && this.state.box.files.map(file => (
-                        <li>
+                        <li key={ file._id }>
                             <a className="fileInfo" href={ file.url }>
                                 <MdInsertDriveFile size={24} color="#A5Cfff" />
                                 <strong>{ file.title }</strong>
