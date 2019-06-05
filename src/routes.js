d42477b55f2823e3a7000b981578a5e6d40b07dd
Felipe Barrella <felipe.netto@mobiauto.com.br>
2019-06-05 17:17:28 -0300

    removed wakemydyno route

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
 import Box from "./pages/Box";
-import Wakemydyno from "./pages/Wakemydyno";
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Main} />
             <Route path="/box/:id" component={Box} />
-            <Route path="/wakemydyno.txt" component={Wakemydyno} />
         </Switch>
     </BrowserRouter>
 );
